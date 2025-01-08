77968a328017a5fba53e39880719d2ac77ed5954
limerri <kovaleva.a2006@yandex.ru>
2025-01-08 19:24:24 +0400

    contacts page is done

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,32 @@
 import "./Contact.scss";
 import { PageInfo } from './../../components/PageInfo/PageInfo';
+import { SectionHeader } from './../../components/SectionHeader/SectionHeader';
+import { useState } from "react";
 
 const Contact = () => {
+    const [list, setList] = useState([
+        {
+            id: 0,
+            name: "Jakarta",
+            text: "Customize your interior design into a dream place with the best designers and quality furniture. We try our best to fulfill your expectations."
+        },
+        {
+            id: 1,
+            name: "Surakarta",
+            text: "Customize your interior design into a dream place with the best designers and quality furniture. We try our best to fulfill your expectations."
+        },
+        {
+            id: 3,
+            name: "Yogyakarta",
+            text: "Customize your interior design into a dream place with the best designers and quality furniture. We try our best to fulfill your expectations."
+        },
+        {
+            id: 4,
+            name: "Bandung",
+            text: "Customize your interior design into a dream place with the best designers and quality furniture. We try our best to fulfill your expectations."
+        }
+    ]);
+
     return (
         <div className="contact-page__container">
             <PageInfo 
@@ -55,8 +80,8 @@ const Contact = () => {
                     <div className="contact-page__form__item">
                         <label for="message" className="contact-page__form__item-label">Message</label>
 
-                        <div className="contact-page__form__item-block">
-                        <input className="contact-page__form__item-input" type="text" id="message" placeholder="Message"></input>   
+                        <div className="contact-page__form__item-block block-message">
+                        <textarea className="contact-page__form__item-input" type="text" id="message" placeholder="Message"></textarea>   
                         </div>                        
                     </div>
 
@@ -65,6 +90,44 @@ const Contact = () => {
                     </div>
                 </div>
             </form>
+
+            <div className="contact-page__location">
+                <SectionHeader name="LOCATION" />
+
+                <div className="contact-page__location__info">
+                    <div className="contact-page__location-left">
+                        <div className="contact-page__location__header">
+                            <span className="contact-page__location__header-title">Visit Our Stores</span>
+
+                            <p className="contact-page__location__header-text">Find us at these locations.</p>
+                        </div>
+
+                        <div className="contact-page__location__email">
+                            <span className="contact-page__location__email-header">Email</span>
+
+                            <span className="contact-page__location__email-text">dananz@gmail.com</span>
+                        </div>
+
+                        <div className="contact-page__location__phone">
+                            <span className="contact-page__location__phone-header">Phone</span>
+
+                            <span className="contact-page__location__phone-number">+62 815 002 1000</span>
+                        </div>
+                    </div>
+
+                    <div className="contact-page__location-right">
+                        {list.map(item => {
+                            return (
+                                <div className="contact-page__location__card">
+                                    <span className="contact-page__location__card-header">{item.name}</span>
+
+                                    <p className="contact-page__location__card-text">{item.text}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
